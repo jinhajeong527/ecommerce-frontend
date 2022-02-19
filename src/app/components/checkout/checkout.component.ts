f3f05a109047bb63e516111de8e2954e9b611025
jinha <jinha4ever@gmail.com>
2022-02-19 11:55:58 +0900

    체크아웃 밸리데이션 이름 및 이메일 추가.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
@@ -29,10 +29,12 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName:[''],
-        lastName:[''],
-        email:['']
-      }),
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email:new FormControl('',
+                              [Validators.required, 
+                               Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+        }),
       shippingAddress: this.formBuilder.group({
         street: [''],
         city: [''],
@@ -80,6 +82,11 @@ export class CheckoutComponent implements OnInit {
       }
     );
   }
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+  
+
   copyShippingAddressToBillingAddress(event){
     if(event.target!.checked){
       this.checkoutFormGroup.controls.billingAddress
@@ -97,6 +104,10 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit(){
     console.log("handling the submit button");
+
+    if(this.checkoutFormGroup.invalid){//form validation이 invalid 하면
+      this.checkoutFormGroup.markAllAsTouched();
+    }
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log(this.checkoutFormGroup.get('customer')?.value.email);
     console.log("shipping address country is "+this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
